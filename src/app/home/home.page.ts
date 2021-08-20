2c0230da157349941aeac64ef50df63bc9c83b17
emnljs <emnljs@gmail.com>
2021-08-20 19:21:40 +0800

    added api, consume data to post and stories

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  users$: Observable<any>;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.users$ = this.userService.getUsers$();
   }
-
 }
